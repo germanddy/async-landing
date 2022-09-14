e3a167c9063c9371488480d1b140b7415b796d3a
Germanddy Hernández <germanddy@gmail.com>
2022-09-14 16:14:26 -0600

    actualizar e GH

---
+++ src/assets/main.js
@@ -0,0 +1,44 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCduj4iwnHq6dfvGXd_iL8_Q&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1d50fe8b1fmshb043c96e207cec3p137891jsn1a45d2a48c43',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi){
+    const response = await fetch(urlApi, options)
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try{
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video =>`
+            <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+                </h3>
+            </div>
+            </div>
+        `)}           
+        `;
+        content.innerHTML = view;
+
+    } catch (error){
+        console.log(error);
+    }
+
+})();
